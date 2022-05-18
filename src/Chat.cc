6050d3e1976b15e7d97f3d324c433dbe63fd2c28
edekadt <edekadt@ucm.es>
2022-05-18 12:46:38 +0200

    Copiado punto de partida de P2.2

---
+++ src/Chat.cc
@@ -0,0 +1,172 @@
+#include "Chat.h"
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+void ChatMessage::to_bin()
+{
+    alloc_data(MESSAGE_SIZE);
+
+    memset(_data, 0, MESSAGE_SIZE);
+
+    //Serializar los campos type, nick y message en el buffer _data
+    char* tmp = _data;
+    memcpy(tmp, &type, sizeof(uint8_t));
+    tmp += sizeof(uint8_t);
+    nick[nick.size()+1]='\0';
+    memcpy(tmp, nick.c_str(), sizeof(char) * 8);
+    tmp += sizeof(char) * 8;
+    message[message.size()+1]='\0';
+    memcpy(tmp, message.c_str(), sizeof(char) * 80);
+}
+
+int ChatMessage::from_bin(char * bobj)
+{
+    alloc_data(MESSAGE_SIZE);
+
+    memcpy(static_cast<void *>(_data), bobj, MESSAGE_SIZE);
+
+    //Reconstruir la clase usando el buffer _data
+    char* tmp = _data;
+
+    memcpy(&type, tmp,sizeof(int8_t));
+    tmp += sizeof(int8_t);
+    nick.resize(sizeof(char) * 8);
+    memcpy(&nick[0], tmp, sizeof(char) * 8);
+    tmp += sizeof(char) * 8;
+    message.resize(sizeof(char) * 80);
+    memcpy(&message[0], tmp, sizeof(char) * 80);
+    return 0;
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+void ChatServer::do_messages()
+{
+    while (true)
+    {
+        
+        Socket* client = 0;
+        ChatMessage msg;
+        int isMessage = socket.recv(msg, client);
+
+        if (isMessage != -1)
+        {
+            std::cout<<"something";
+            switch(msg.type)
+            {
+                case ChatMessage::LOGIN:
+                    clients.push_back(std::unique_ptr<Socket>(client));
+                    std::cout << msg.nick.c_str() << " logged in" << std::endl;
+                break;
+                case ChatMessage::LOGOUT:
+                {
+                    auto it = clients.begin();
+                        
+                    while (it != clients.end() && (**it !=  *client))
+                    {
+                        ++it;
+                    }
+                    
+                    if (it == clients.end())
+                    {
+                        std::cout << "Jugador desconectado" << std::endl;
+                    }
+                    else
+                    {
+                        std::cout << "Jugador desconectandose: " << msg.nick << std::endl;
+                        clients.erase(it);                 
+                        Socket *delSock = (*it).release(); 
+                        delete delSock;                    
+                    }
+                }
+                break;
+                case ChatMessage::MESSAGE:
+                    for (auto it = clients.begin(); it != clients.end(); it++)
+                        {
+                            if (**it !=  *client)
+                            {
+                                socket.send(msg, **it);
+                            }
+                        }
+
+                    std::cout << msg.nick.c_str() << " sent a message" << std::endl;
+                break;
+            }
+
+            std::cout << "Conected: " << clients.size() << std::endl;
+        }   
+    }
+}
+
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+
+
+ChatClient::ChatClient(const char * s, const char * p, const char * n):socket(s, p),
+        nick(n)
+        {socket.printsd(); };
+
+void ChatClient::login()
+{
+    std::cout<<"Logging in\n";
+    std::string msg;
+
+    ChatMessage em(nick, msg);
+    em.type = ChatMessage::LOGIN;
+
+    socket.send(em, socket);
+}
+
+void ChatClient::logout()
+{
+    std::string msg;
+
+    ChatMessage em(nick, msg);
+    em.type = ChatMessage::LOGOUT;
+
+    socket.send(em, socket);
+}
+
+void ChatClient::input_thread()
+{
+    while (true)
+    {
+        std::string msg; 
+        // Leer stdin con std::getline
+        // Enviar al servidor usando socket
+        std::getline(std::cin, msg, '\n');
+        
+        if(msg == "LOGOUT") 
+        {
+            logout();
+            break;
+        }
+        else
+        {
+            ChatMessage em (nick, msg);
+            em.type = ChatMessage::MESSAGE;
+            socket.send(em, socket);
+        }
+    }
+}
+
+void ChatClient::net_thread()
+{
+    while(true)
+    {
+        //Recibir Mensajes de red
+        //Mostrar en pantalla el mensaje de la forma "nick: mensaje"
+        ChatMessage em;
+
+        int isMessage = socket.recv(em);
+
+        if (isMessage != -1 && em.nick == nick)
+            std::cout<<"same nick\n";
+
+        if(isMessage != -1 && em.nick != nick)
+            std::cout << "<" << em.nick << ">: " << em.message << std::endl;
+    }
+}
+
